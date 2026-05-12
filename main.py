68f4268044ae0d45a533b92cf70383e97c953cc5
temp297 <temp@nniif.org.ua>
2026-05-12 20:45:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -801,7 +801,7 @@ async def main():
     port = int(os.environ.get("PORT", 8000))
     site = web.TCPSite(runner, '0.0.0.0', port)
     await site.start()
-    
+
     await asyncio.Event().wait()
 
 if __name__ == "__main__":
