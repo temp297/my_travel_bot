940e26368a67acede3e6da5e4f79997f98c07be7
temp297 <temp@nniif.org.ua>
2026-06-11 23:12:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ try:
     FEEDBACK_HOUR = int(os.getenv("FEEDBACK_HOUR", "11"))
     FEEDBACK_MINUTE = int(os.getenv("FEEDBACK_MINUTE", "0"))
     ASSISTANT_HOUR = int(os.getenv("ASSISTANT_HOUR", "23"))
-    ASSISTANT_MINUTE = int(os.getenv("ASSISTANT_MINUTE", "5"))
+    ASSISTANT_MINUTE = int(os.getenv("ASSISTANT_MINUTE", "30"))
 except ValueError:
     raise ValueError("ADMIN_ID, REVIEWS_CHAT_ID, FEEDBACK_HOUR та FEEDBACK_MINUTE мають бути цілими числами!")
 
